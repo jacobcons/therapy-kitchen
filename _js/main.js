33296f322e9e6e70bf6a3455a42b9e646a667702
Jacob Cons <jacobcons@gmail.com>
2018-07-10 15:44:35 +0100

    service pages

--- _js/main.js
+++ _js/main.js
@@ -3,7 +3,25 @@ import Fetch from './Fetch.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
 	if (window.location.pathname === '/') {
-	} else if (window.location.pathname === '/portfolio.html') {
+	} else if (window.location.pathname === '/services/kitchen-therapy.html' || window.location.pathname === '/services/cookery.html') {
+		const slider = tns({
+			container: '.js-slider-triples',
+			items: 1,
+			nav: false,
+			autoplayButtonOutput: false,
+			controlsText: [`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
+			</svg>`, `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+			</svg>`],
+			lazyload: true,
+			gutter: 8,
+			responsive: {
+				592: {
+					fixedWidth: 592,
+				},
+			},
+		});
 	} else if (window.location.pathname === '/contact.html') {
 		const elTextInputs = document.querySelectorAll('.js-text-input')
 		elTextInputs.forEach((input) => {
--- main.js
+++ main.js
@@ -28,7 +28,26 @@ var _Fetch2 = _interopRequireDefault(_Fetch);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 document.addEventListener('DOMContentLoaded', async () => {
-	if (window.location.pathname === '/') {} else if (window.location.pathname === '/portfolio.html') {} else if (window.location.pathname === '/contact.html') {
+	if (window.location.pathname === '/') {} else if (window.location.pathname === '/services/kitchen-therapy.html' || window.location.pathname === '/services/cookery.html') {
+		const slider = (0, _tinySlider.tns)({
+			container: '.js-slider-triples',
+			items: 1,
+			nav: false,
+			autoplayButtonOutput: false,
+			controlsText: [`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
+			</svg>`, `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
+			</svg>`],
+			lazyload: true,
+			gutter: 8,
+			responsive: {
+				592: {
+					fixedWidth: 592
+				}
+			}
+		});
+	} else if (window.location.pathname === '/contact.html') {
 		const elTextInputs = document.querySelectorAll('.js-text-input');
 		elTextInputs.forEach(input => {
 			input.addEventListener('focus', () => input.parentElement.classList.add('input-field--active'));
