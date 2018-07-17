c8166d9dfc9324329f24b7a991c2a53fb6086ac1
Jacob Cons <jacobcons@gmail.com>
2018-07-17 13:00:59 +0100

    blog filters

--- _js/main.js
+++ _js/main.js
@@ -24,8 +24,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 		elHamburgerMenu.classList.toggle('hamburger-menu--is-active');
 	});
 	
-	if (window.location.pathname === '/') {
-	} else if (window.location.pathname === '/services/kitchen-therapy.html' || window.location.pathname === '/services/cookery.html') {
+	if (window.location.pathname === '/services/kitchen-therapy.html' || window.location.pathname === '/services/cookery.html') {
 		const slider = tns({
 			container: '.js-slider-triples',
 			items: 1,
@@ -45,15 +44,69 @@ document.addEventListener('DOMContentLoaded', async () => {
 			},
 		});
 		
-		document.querySelectorAll('.js-testimonial-more').forEach(el => {
+		const elsExpandTestimonial = document.querySelectorAll('.js-testimonial-more')
+		elsExpandTestimonial.forEach(el => {
 			el.addEventListener('click', (e) => {
 				const fullQuote = e.target.getAttribute('data-full-quote')
 				spawnModal(`<p>"${fullQuote}"</p>`)
 			})
 		})
+	} else if (window.location.pathname === '/blog.html') {
+		//Build objects containing filter as key and indexes of cards of said filter as value 
+		const elsBlogCard = document.querySelectorAll('.js-blog-card');
+		let dateIndexes = {}
+		let tagIndexes = {}
+		elsBlogCard.forEach((card, i) => {
+			const date = card.getAttribute('data-date')
+			if (typeof dateIndexes[date] === 'undefined') {
+				dateIndexes[date] = [i]
+			} else {
+				dateIndexes[date].push(i)
+			}
+			
+			const tags = card.getAttribute('data-tags').split(',')
+			tags.forEach((tag) => {
+				if (typeof tagIndexes[tag] === 'undefined') {
+					tagIndexes[tag] = [i]
+				} else {
+					tagIndexes[tag].push(i)
+				}
+			})
+		})
+		
+		//Show cards corresponding to a clicked filter
+		const elsFilter = document.querySelectorAll('.js-filter')
+		elsFilter.forEach((filter) => {
+			filter.addEventListener('click', (e) => {
+				const date = filter.getAttribute('data-date')
+				const tag = filter.getAttribute('data-tag')
+				let indexesToShow
+				//Get indexes of cards that should be shown
+				if (date !== null) {
+					indexesToShow = dateIndexes[date]
+				} else if (tag !== null) {
+					indexesToShow = tagIndexes[tag]
+				}
+				
+				//Build array of bools representing whether each card should be shown
+				let cardsToShow = new Array(elsBlogCard.length).fill(false)
+				indexesToShow.forEach((index) => {
+					cardsToShow[index] = true
+				})
+				
+				//Use array of bools to hide/show cards
+				cardsToShow.forEach((show, i) => {
+					if (show === true) {
+						elsBlogCard[i].classList.remove('hide')
+					} else if (show === false) {
+						elsBlogCard[i].classList.add('hide')
+					}
+				})
+			})
+		})
 	} else if (window.location.pathname === '/contact.html') {
-		const elTextInputs = document.querySelectorAll('.js-text-input')
-		elTextInputs.forEach((input) => {
+		const elsTextInput = document.querySelectorAll('.js-text-input')
+		elsTextInput.forEach((input) => {
 			input.addEventListener('focus', () => input.parentElement.classList.add('input-field--active'))
 			input.addEventListener('blur', () => {
 				if (!input.value) input.parentElement.classList.remove('input-field--active');
@@ -64,11 +117,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 		elSendBtn.addEventListener('click', async (e) => {
 			e.preventDefault()
 			const elForm = document.querySelector('.js-contact-form')
-			const elRequiredInputs = document.querySelectorAll('.js-required-input')
+			const elsRequiredInputs = document.querySelectorAll('.js-required-input')
 			const elEmailInput = document.querySelector('input[name="email"]')
-			const elErrors = document.querySelectorAll('.js-input-error')
+			const elsError = document.querySelectorAll('.js-input-error')
 			
-			elErrors.forEach((error) => error.remove())
+			elsError.forEach((error) => error.remove())
 			
 			let errors = {}
 			if (!(/.+@.+/.test(elEmailInput.value))) {
@@ -77,7 +130,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 					input: elEmailInput,
 				}
 			}
-			elRequiredInputs.forEach((input) => {
+			elsRequiredInput.forEach((input) => {
 				if (!input.value) {
 					errors[input.name] = {
 						text: 'Please fill out the required field',
